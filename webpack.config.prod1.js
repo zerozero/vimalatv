14dbbe14dea0f4334591a1073a59cc9f77c62f9b
Jon Rowe <mail@jonrowe.com>
2017-07-20 11:37:59 +0100

    start minifying

--- webpack.config.prod1.js
+++ webpack.config.prod1.js
@@ -1,34 +1,10 @@
 var path = require('path');
 const AotPlugin = require('@ngtools/webpack').AotPlugin;
 const helpers = require('./helpers');
+var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.config.common.js');
-var aotPlugin = new AotPlugin({
-    tsConfigPath: './tsconfig.prod.json',
-    entryModule: helpers.root('assets/app/app.module#AppModule'),
-    compilerOptions: {
-        genDir: helpers.root('aot'),
-        skipCodeGeneration: false
-    }
-});
 
-// aotPlugin._compilerHost._resolve = function(path_to_resolve) {
-//     path_1 = require("path");
-//     path_to_resolve = aotPlugin._compilerHost._normalizePath(path_to_resolve);
-//     if (path_to_resolve[0] == '.') {
-//         return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost.getCurrentDirectory(), path_to_resolve));
-//     }
-//     else if (path_to_resolve[0] == '/' || path_to_resolve.match(/^\w:\//)) {
-//         return path_to_resolve;
-//     }
-//     else {
-//         // console.log("basePath: "+aotPlugin._compilerHost._basePath);
-//         // console.log("path: "+path_to_resolve);
-//         var p = aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost._basePath, path_to_resolve));
-//         console.log(p);
-//         return p;
-//     }
-// };
 
 module.exports = webpackMerge( commonConfig, {
     devtool: 'cheap-module-eval-source-map',
@@ -45,6 +21,28 @@ module.exports = webpackMerge( commonConfig, {
         ]
     },
     plugins: [
-        aotPlugin
+        new AotPlugin({
+            tsConfigPath: './tsconfig.prod.json',
+            entryModule: helpers.root('assets/app/app.module#AppModule'),
+            compilerOptions: {
+                genDir: 'aot/',
+                skipCodeGeneration: false
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'styles', 'polyfills']
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            comments: false,
+            compress: {
+                screw_ie8: true,
+                warnings: false
+            },
+            mangle: {
+                keep_fnames: true,
+                screw_i8: true
+            }
+        })
     ]
 });
